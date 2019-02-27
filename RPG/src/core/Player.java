7953c4dc9b9a3f735a16962035e596cad802fdad
Bernardo <Bernardofranceschina@gmail.com>
2019-02-27 14:11:49 -0300

    New player class

---
+++ RPG/src/core/Player.java
@@ -0,0 +1,9 @@
+package core;
+
+public class Player extends GameObject {
+
+    public Player(String name, String description, int id, double size) {
+        super(name, description, id, size);
+    }
+    
+}
