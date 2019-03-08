68f588424b53a290d8e83fb19124e45243fade50
ErikIPU2 <erikborella@gmail.com>
2019-03-08 15:23:26 -0300

    Map template gerator

--- RPG/src/core/map/MapCreator.java
+++ RPG/src/core/map/MapCreator.java
@@ -1,4 +1,54 @@
 package core.map;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MapCreator {
+
+    /**
+     * Gera um arquivo com um template pronto para o mapa de acordo com o tamanho dele
+     * @param filePath Localização da criação do arquivo
+     * @param height altura do mapa
+     * @param weight largura do mapa
+     * @return Se um arquivo com o mesmo nome existir, retorna false e não gera nada. Se não retorna true e cria o arquivo
+     */
+    public static boolean gerateTemplate(File filePath, int height, int weight) {
+        if (filePath.exists()) {
+            return false;
+        }
+        StringBuilder temp = new StringBuilder(String.format("(%d, %d)\n", height, weight));
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < weight; j++) {
+                if (i == 0 && j == 0) {
+                    temp.append("┌");
+                } else if (i == 0 && j == weight-1) {
+                    temp.append("┐");
+                } else if (i == height-1 && j == 0) {
+                    temp.append("└");
+                } else if (i == height-1 && j == weight-1) {
+                    temp.append("┘");
+                } else if (i == 0 || i == height-1) {
+                    temp.append("─");
+                } else if (j == 0 || j == weight-1) {
+                    temp.append("│");
+                } else {
+                    temp.append(" ");
+                }
+            }
+            temp.append("\n");
+        }
+
+        try {
+            FileWriter a = new FileWriter(filePath);
+            a.write(temp.toString());
+            a.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        System.out.printf("Map template %s {%d, %d} create\n", filePath.getName(), height, weight);
+        return true;
+    }
 }
--- RPG/src/tests/runer.java
+++ RPG/src/tests/runer.java
@@ -5,6 +5,7 @@ import core.Inventory;
 import core.Player;
 import core.idControl.LoadJsonObject;
 import core.Dice;
+import core.map.MapCreator;
 import core.weapons.Melee;
 
 import java.io.File;
@@ -13,29 +14,30 @@ import java.util.Scanner;
 public class runer {
 
     public static void main(String[] args) {
-        LoadJsonObject loadJsonObject = new LoadJsonObject(
-                new File("/home/erik/Documentos/RPG_engine_commandLine/RPG/src/core/weapons/weaponsArray.json"));
-        loadJsonObject.load();
-
-        Scanner input = new Scanner(System.in);
-        System.out.println("Digite o nome do seu personagem:");
-        String name = input.nextLine();
-        System.out.println("Agora me conte um pouco dele: ");
-        String desc = input.nextLine();
-
-        Player p = new Player(name, desc, 1000, 100, 100, 100,
-                100, 10, new Inventory(11));
-
-        CommandInterpreter commandInterpreter = new CommandInterpreter(p);
-
-        String command = "";
-        while (!command.equals("sair")) {
-            if (commandInterpreter.isInventoryOpen()) {
-                System.out.println("** Seu iventario esta aberto");
-            }
-            System.out.println("me diga o que fazer");
-            command = input.nextLine();
-            System.out.println(commandInterpreter.run(command));
-        }
+        MapCreator.gerateTemplate(new File("/home/erik/Documentos/RPG_engine_commandLine/RPG/src/mapTest/testMap.map"), 10, 50);
+//        LoadJsonObject loadJsonObject = new LoadJsonObject(
+//                new File("/home/erik/Documentos/RPG_engine_commandLine/RPG/src/core/weapons/weaponsArray.json"));
+//        loadJsonObject.load();
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Digite o nome do seu personagem:");
+//        String name = input.nextLine();
+//        System.out.println("Agora me conte um pouco dele: ");
+//        String desc = input.nextLine();
+//
+//        Player p = new Player(name, desc, 1000, 100, 100, 100,
+//                100, 10, new Inventory(11));
+//
+//        CommandInterpreter commandInterpreter = new CommandInterpreter(p);
+//
+//        String command = "";
+//        while (!command.equals("sair")) {
+//            if (commandInterpreter.isInventoryOpen()) {
+//                System.out.println("** Seu iventario esta aberto");
+//            }
+//            System.out.println("me diga o que fazer");
+//            command = input.nextLine();
+//            System.out.println(commandInterpreter.run(command));
+//        }
     }
 }
